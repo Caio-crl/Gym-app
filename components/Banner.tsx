1b4434b32e5647d6b1764c0407e1c1527e782213
Caio-crl <caioriguera@gmail.com>
2023-10-30 11:05:15 -0300

    test change

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -29,7 +29,7 @@ export const Banner: React.FC<BannerProps> = ({
 					style={{
 						width: '70%',
 						marginLeft: 335,
-						paddingTop: 12,
+						paddingTop: 11,
 					}}
 				>
 					{rightIcon}
